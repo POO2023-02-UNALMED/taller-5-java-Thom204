4e966fdeaa55e36300a57d0b6c4eb0a434c4878d
Thom204 <129136707+Thom204@users.noreply.github.com>
2023-09-26 07:15:13 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -70,10 +70,10 @@ public class Animal {
     }
     public String toString(){
         if (zona==null) {
-            return "Mi nombre es "+this.nombre+"tengo una edad de "+this.edad+", habito en "+this.habitat+" y mi genero es " + this.genero;
+            return "Mi nombre es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+" y mi genero es " + this.genero;
         }
         else{
-            return "Mi nombre es "+this.nombre+"tengo una edad de "+this.edad+", habito en "+this.habitat+" y mi genero es " +
+            return "Mi nombre es "+this.nombre+", tengo una edad de "+this.edad+", habito en "+this.habitat+" y mi genero es " +
                     this.genero+", la zona en la que me ubico es "+this.zona.getNombre()+", en el "+this.zona.getZoo().getNombre();
         }
     }
